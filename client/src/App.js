980aede8095a5b582848def58ce9c8518268b9bb
DESKTOP-JAJH8A5\Progreanmer <greanmer.tm@gmail.com>
2018-04-24 19:25:22 +0700

    list bear

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {data: []};
+  }
+  componentDidMount(){
+    axios.get('http://localhost:8000/api/bears')
+    .then(result => {
+      this.setState({data: result.data})
+    })
+  }
   render() {
+    let bears = this.state.data;
     return (
-      <div>Hello</div>
+      <div>
+        {
+            bears.map(bear => <div key={bear.id}>{bear.name}</div>)
+        }
+      </div>
     );
   }
 }
-
 export default App;
